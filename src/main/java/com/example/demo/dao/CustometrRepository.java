3219144c7e7a2439c1778807a1ef00afbc56091a
bhagavath <lordcodeking@gmail.com>
2022-10-26 00:06:32 +0530

    added cstmr repo

---
+++ src/main/java/com/example/demo/dao/CustometrRepository.java
@@ -0,0 +1,45 @@
+package com.example.demo.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.models.Customer;
+
+@Repository
+public class CustometrRepository {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    public void addCustomer(Customer customer){
+        String sql_query = "INSERT INTO Customer VALUES(?, ?, ?, ?, ?)";
+        jdbcTemplate.update(
+                sql_query,
+                customer.getCustomerId(),
+                customer.getFirstName(),
+                customer.getLastName(),
+                customer.getUserName(),
+                customer.getPassword()
+        );
+
+    }
+
+    public void updateUserName(Customer customer){
+        String sql_query = "Update Customer SET  userName = ?, password = ? WHERE customerID = ?";
+        jdbcTemplate.update(sql_query,
+                customer.getUserName(),
+                customer.getCustomerId()
+        );
+    }
+
+    public void updatPassword(Customer customer){
+        String sql_query = "Update Customer SET  userName = ?, password = ? WHERE customerID = ?";
+        jdbcTemplate.update(sql_query,
+                customer.getUserName(),
+                customer.getCustomerId()
+        );
+    }
+
+
+}
