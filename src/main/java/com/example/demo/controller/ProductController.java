1f88a16c29d83e1097b04fe4ef36e6c2ff886130
cokepranav <v.saiprani123@gmail.com>
2022-10-26 21:29:42 +0530

    product Controller and home

---
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -0,0 +1,34 @@
+package com.example.demo.controller;
+
+import com.example.demo.dao.ProductRepository;
+import com.example.demo.models.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+
+@Controller
+public class ProductController {
+    private final ProductRepository productRepository;
+
+    @Autowired
+    public ProductController(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @PostMapping("/Product")
+//    @GetMapping({"/mouryaBC"})
+    public String setProduct(Product product){
+        productRepository.createProduct(product);
+//        System.out.println("lol");
+        return "create done amma next!!";
+    }
+
+    @PutMapping("/Product")
+    public String updateProduct(Product product){
+        productRepository.updateProduct(product);
+        return "update ayyindi amma next!!";
+    }
+
+}
